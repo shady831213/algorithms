576e0b68d131be45d9a97750c58d15e93e69b7bf
Ubuntu <shady831213>
2018-02-01 19:19:03 -0800

    add golang slice comments

--- sort/heapSort.go
+++ sort/heapSort.go
@@ -6,6 +6,13 @@ T(n) = n*sum(0/2+1/2^1+...+lgn/2^(lgn+1))= (n/2)*sum(2/2^1+...+lgn/2^lgn)
 d heap:
 n*(d-1)/d^(h+1), max h is logd((d-1)n), T(n) = (n*(d-1)/d) *sum(1/d^1+2/d^2+...logd((d-1)n)/d^logd(d-1)n)
 = (n*(d-1)/d) <= O(n)
+
+about golang slice:
+a := []int{...}
+b := a
+
+b and a point to the same memory, but are different object.
+so they both can modify shared data.But they have different lenth,index and so on
 */
 
 package sort
