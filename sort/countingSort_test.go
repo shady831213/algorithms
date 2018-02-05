392ef91efbecd9f231efa7cd1becfbda33049b15
Ubuntu <shady831213>
2018-02-04 18:41:43 -0800

    add countingSort

---
+++ sort/countingSort_test.go
@@ -0,0 +1,21 @@
+package sort
+
+import (
+	"algorithms/sort/heap"
+	"testing"
+)
+
+func countingSortWrapper(arr []int) {
+	_arr := make([]int, len(arr), cap(arr))
+	copy(_arr, arr)
+	a := heapIntArray(arr)
+	h := heap.Heap{&a}
+	h.BuildHeap()
+	max := h.Pop().(int)
+	sortedArry := countingSort(arr, max)
+	copy(arr, sortedArry)
+}
+
+func Test_countingSort(t *testing.T){
+	testSort(t, countingSortWrapper)
+}
