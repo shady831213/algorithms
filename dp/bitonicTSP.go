214aabdde00322c9ca82637d9433e5d19de7cb2a
Ubuntu <shady831213>
2018-02-26 18:19:55 -0800

    add bitonicTSP best patch output

--- dp/bitonicTSP.go
+++ dp/bitonicTSP.go
@@ -3,14 +3,34 @@ package dp
 import (
 	"math"
 	"sort"
+	"fmt"
 )
 
 type point struct {
 	x, y float64
 }
 
-func (p *point) dist(p1 *point) (float64) {
-	return math.Sqrt(math.Pow(p.x-p1.x, 2) + math.Pow(p.y-p1.y, 2))
+func newPoint(x, y float64) *point {
+	return &point{x, y}
+}
+
+type line struct {
+	p0,p1 *point
+}
+
+func newLine(p0,p1 *point) *line {
+	return &line{p0,p1}
+}
+
+func (l *line) dist() (float64) {
+	return math.Sqrt(math.Pow(l.p0.x-l.p1.x, 2) + math.Pow(l.p0.y-l.p1.y, 2))
+}
+
+func (l *line) print() {
+	fmt.Println("----")
+	fmt.Println(l.p0)
+	fmt.Println(l.p1)
+	fmt.Println("----")
 }
 
 type pointSlice []*point
@@ -23,29 +43,48 @@ func (p *pointSlice) Swap(i, j int) {
 	(*p)[i], (*p)[j] = (*p)[j], (*p)[i]
 }
 
-func bitonicTSP(points []*point) (float64) {
-	if len(points) < 3 {
-		panic("There must be more than 3 points")
+func bitonicTSP(points []*point) (float64, []*line) {
+	if len(points) < 2 {
+		panic("There must be more than 2 points")
 	}
 
 	sortedPoints := pointSlice(points)
 	sort.Slice(points, sortedPoints.Less)
+
+	//side data structures
 	dist := make([][]float64, len(sortedPoints), cap(sortedPoints))
+	lines := make([][][]*line, len(sortedPoints), cap(sortedPoints))
 	for i := range dist {
 		dist[i] = make([]float64, len(sortedPoints), cap(sortedPoints))
+		lines[i] = make([][]*line, len(sortedPoints), cap(sortedPoints))
 	}
+
 	for numPoints := 1; numPoints < len(sortedPoints); numPoints++ {
+		//idxPoint < numPoints-1
 		for idxPoint := 0; idxPoint < numPoints-1; idxPoint++ {
-			dist[numPoints][idxPoint] = dist[numPoints-1][idxPoint] + sortedPoints[numPoints-1].dist(sortedPoints[numPoints])
+			line := newLine(sortedPoints[numPoints-1], sortedPoints[numPoints])
+			dist[numPoints][idxPoint] = dist[numPoints-1][idxPoint] + line.dist()
+			lines[numPoints][idxPoint] = append(lines[numPoints-1][idxPoint], line)
 		}
-		dist[numPoints][numPoints-1] = dist[numPoints-1][0] + sortedPoints[numPoints].dist(sortedPoints[0])
+
+		//idxPoint == numPoints-1
+		line := newLine(sortedPoints[0], sortedPoints[numPoints])
+		dist[numPoints][numPoints-1] = dist[numPoints-1][0] + line.dist()
+		lines[numPoints][numPoints-1] = append(lines[numPoints-1][0],  line)
 		for idxPreviousPoint := 0; idxPreviousPoint < numPoints-1; idxPreviousPoint++ {
-			temp := dist[numPoints-1][idxPreviousPoint] + sortedPoints[numPoints].dist(sortedPoints[idxPreviousPoint])
+			line := newLine(sortedPoints[idxPreviousPoint], sortedPoints[numPoints])
+			temp := dist[numPoints-1][idxPreviousPoint] + line.dist()
 			if temp < dist[numPoints][numPoints-1] {
 				dist[numPoints][numPoints-1] = temp
+				lines[numPoints][numPoints-1] = append(lines[numPoints-1][idxPreviousPoint],  line)
 			}
 		}
-		dist[numPoints][numPoints] = dist[numPoints][numPoints-1] + sortedPoints[numPoints-1].dist(sortedPoints[numPoints])
+
+		//idxPoint == numPoints
+		line = newLine(sortedPoints[numPoints-1], sortedPoints[numPoints])
+		dist[numPoints][numPoints] = dist[numPoints][numPoints-1] + line.dist()
+		lines[numPoints][numPoints] = append(lines[numPoints][numPoints-1],line)
 	}
-	return dist[len(sortedPoints)-1][len(sortedPoints)-1]
+
+	return dist[len(sortedPoints)-1][len(sortedPoints)-1], lines[len(sortedPoints)-1][len(sortedPoints)-1]
 }
--- dp/bitonicTSP_test.go
+++ dp/bitonicTSP_test.go
@@ -4,14 +4,21 @@ import (
 	"testing"
 	"fmt"
 	"math"
+	"reflect"
 )
 
 func TestBitonicTSP(t *testing.T)  {
-	points := []*point{&point{0,6}, &point{1,0},&point{2,3},
-	&point{5,4},&point{6,1},&point{7,5},&point{8,2}}
-	result := bitonicTSP(points)
+	points := []*point{newPoint(0,6), newPoint(1,0),newPoint(2,3),
+		newPoint(5,4),newPoint(6,1),newPoint(7,5),newPoint(8,2)}
+	expPath := []*line{newLine(points[0], points[1]), newLine(points[0], points[2]), newLine(points[2], points[3]),
+		newLine(points[1], points[4]), newLine(points[3], points[5]), newLine(points[4], points[6]), newLine(points[5], points[6])}
+	result, path := bitonicTSP(points)
 	if math.Abs(result - 25.584025) >= 0.0001 {
 		t.Log(fmt.Sprintf("expect 25.584025, but get %f", result))
+		t.Fail()
+	}
+	if !reflect.DeepEqual(path, expPath) {
+		t.Log("Path not match!")
 		t.Fail()
 	}
 }
