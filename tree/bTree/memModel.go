f873005250969fd43bfa551025167a51cf8a4778
Ubuntu <shady831213>
2018-03-12 01:01:59 -0700

    add memModel for bTree

---
+++ tree/bTree/memModel.go
@@ -0,0 +1,102 @@
+package bTree
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type memory interface {
+	write(interface{}, interface{})
+	read(interface{}) (interface{}, bool)
+}
+
+type Memory interface {
+	memory
+	encIdx(interface{}) (interface{})
+	decIdx(interface{}) (interface{})
+	encData(interface{}) interface{}
+	decData(interface{}) interface{}
+}
+
+type discModel struct {
+	mem map[int][]byte // (id, []byte)
+	memory
+}
+
+func (m *discModel) init() *discModel {
+	m.mem = make(map[int][]byte)
+	return m
+}
+
+func (m *discModel) write(id interface{}, data interface{}) {
+	m.mem[id.(int)] = data.([]byte)
+}
+
+func (m *discModel) read(id interface{}) (interface{}, bool) {
+	data, ok := m.mem[id.(int)]
+	return data, ok
+}
+
+func newDisc() *discModel {
+	return new(discModel).init()
+}
+
+//LRU
+type cacheItem struct {
+	key, value interface{}
+	dirty      bool
+}
+
+type cacheModel struct {
+	mem             map[interface{}]*list.Element
+	pq              *list.List
+	downStreamModel memory
+	Memory
+}
+
+func (m *cacheModel) init(size int, downStreamModel memory, self Memory) (*cacheModel) {
+	m.mem = make(map[interface{}]*list.Element)
+	m.pq = list.New()
+	for i := 0; i < size; i++ {
+		cacheItem := new(cacheItem)
+		cacheItem.dirty = false
+		m.pq.PushBack(cacheItem)
+	}
+	m.downStreamModel = downStreamModel
+	m.Memory = self
+	return m
+}
+
+func (m *cacheModel) update(id interface{}) (*list.Element) {
+	var item *list.Element
+	var ok bool
+	if item, ok = m.mem[id]; !ok {
+		if item = m.pq.Back(); item.Value.(*cacheItem).dirty {
+			m.downStreamModel.write(item.Value.(*cacheItem).key, item.Value.(*cacheItem).value)
+			item.Value.(*cacheItem).dirty = false
+		}
+		delete(m.mem, item.Value.(*cacheItem).key)
+		m.mem[id] = item
+		item.Value.(*cacheItem).key = id
+	}
+	m.pq.MoveToFront(item)
+	return item
+}
+
+func (m *cacheModel) write(id interface{}, data interface{}) {
+	sId, sData := m.Memory.encIdx(id), m.Memory.encData(data)
+	item := m.update(sId)
+	item.Value.(*cacheItem).value = sData
+	item.Value.(*cacheItem).dirty = true
+}
+
+func (m *cacheModel) read(id interface{}) (interface{}, bool) {
+	sId := m.Memory.encIdx(id)
+	item := m.update(sId)
+	var ok bool
+	if item.Value.(*cacheItem).value, ok = m.downStreamModel.read(sId); !ok {
+		panic(fmt.Sprintf("%v is not initialized in downStreamModel", sId))
+	}
+	item.Value.(*cacheItem).dirty = false
+	return m.Memory.decData(item.Value.(*cacheItem).value), ok
+}
---
+++ tree/bTree/memModel_test.go
@@ -0,0 +1,89 @@
+package bTree
+
+import (
+	"testing"
+	"bytes"
+	"encoding/binary"
+	"encoding/gob"
+	"fmt"
+)
+
+func TestDiscModel(t *testing.T) {
+	d := newDisc()
+	for i := 0; i < 8; i ++ {
+		buf := new(bytes.Buffer)
+		binary.Write(buf, binary.LittleEndian, uint32(i))
+		d.write(i, buf.Bytes())
+	}
+	for i := 0; i < 8; i ++ {
+		data, _ := d.read(i)
+		buf := bytes.NewBuffer(data.([]byte))
+		var data_i uint32
+		binary.Read(buf, binary.LittleEndian, &data_i)
+		if uint32(i) != data_i {
+			t.Log(i, data_i)
+			t.Fail()
+		}
+	}
+}
+
+type testCacheModel struct {
+	cacheModel
+}
+
+func (m *testCacheModel) init(size int, downStreamModel memory) (*testCacheModel) {
+	m.cacheModel.init(size, downStreamModel, m)
+	return m
+}
+
+func (c *testCacheModel) encIdx(id interface{}) (interface{}) {
+	return int(id.(uint32))
+}
+
+func (c *testCacheModel) decIdx(id interface{}) (interface{}) {
+	return uint32(id.(int))
+}
+
+func (c *testCacheModel) encData(data interface{}) (interface{}) {
+	buf := bytes.NewBuffer(nil)
+	enc := gob.NewEncoder(buf)
+	enc.Encode(data)
+	return buf.Bytes()
+}
+
+func (c *testCacheModel) decData(data interface{}) (interface{}) {
+	var result string
+	buf := bytes.NewBuffer(data.([]byte))
+	dec := gob.NewDecoder(buf)
+	dec.Decode(&result)
+	return result
+}
+
+func newTestCacheModel(size int, downStreamModel memory) *testCacheModel {
+	return new(testCacheModel).init(size, downStreamModel)
+}
+
+func TestCacheModel(t *testing.T) {
+	d := newDisc()
+	c := newTestCacheModel(4, d)
+	for i := 0; i < 8; i ++ {
+		c.write(uint32(i), string(i))
+	}
+	//check data
+	for i := 0; i < 8; i ++ {
+		data, _ := c.read(uint32(i))
+		if string(i) != data {
+			t.Log(i, data)
+			t.Fail()
+		}
+	}
+	//check pq status
+	expOrder := []int{7,6,5,4}
+	for item, i:= c.pq.Front(), 0; item != nil; item = item.Next() {
+		if item.Value.(*cacheItem).key != expOrder[i] {
+			t.Log(i, fmt.Sprintf("%+v", item.Value))
+			t.Fail()
+		}
+		i++
+	}
+}
