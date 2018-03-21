e8879b1ec385cf4afdb22e4b5f5ec214d6e01019
Ubuntu <shady831213>
2018-03-21 04:58:30 -0700

    continue rs-vEB-Tree

--- tree/vEBTree/rsVEBTree.go
+++ tree/vEBTree/rsVEBTree.go
@@ -2,6 +2,7 @@ package vEBTree
 
 import (
 	"container/list"
+	"math"
 )
 
 type rsVEBTreeMinMax struct {
@@ -21,19 +22,40 @@ func newRsVEBTreeMinMax(key, value interface{}) *rsVEBTreeMinMax {
 }
 
 type rsVEBTreeElement struct {
-	u       int
+	u,summaryU,clusterU       int
 	min     *rsVEBTreeMinMax
 	max     *rsVEBTreeMinMax
 	summary *rsVEBTreeElement
 	cluster map[interface{}]*rsVEBTreeElement
 	Less    func(interface{}, interface{}) bool
+	High    func(int, interface{}) interface{}
+	Low     func(int, interface{}) interface{}
 }
 
-func (e *rsVEBTreeElement) init(u int, Less func(interface{}, interface{}) bool) *rsVEBTreeElement {
+func (e *rsVEBTreeElement) init(u int,
+	Less func(interface{}, interface{}) bool,
+	High func(int, interface{}) interface{},
+	Low func(int, interface{}) interface{}) *rsVEBTreeElement {
 	e.u = u
 	e.Less = Less
+	e.High = High
+	e.Low = Low
 	e.cluster = make(map[interface{}]*rsVEBTreeElement)
+	if e.u > 2 {
+		e.summaryU = int(math.Ceil(math.Sqrt(float64(u))))
+		e.clusterU = int(math.Floor(math.Sqrt(float64(e.u))))
+		e.summary = newRsVEBTreeElement(e.summaryU, Less, High, Low)
+	} else {
+		e.summaryU = 0
+		e.clusterU = 0
+	}
 	return e
+}
+
+func (e *rsVEBTreeElement) addCluster(key interface{}) {
+	if e.u > 2 {
+		e.cluster[key] = newRsVEBTreeElement(e.clusterU, e.Less, e.High, e.Low)
+	}
 }
 
 func (e *rsVEBTreeElement) insertEmptyTree(key, value interface{}) {
@@ -49,7 +71,7 @@ func (e *rsVEBTreeElement) insert(key, value interface{}) {
 	} else {
 		_key, _value := key, value
 		if e.Less(_key, e.min.key) {
-			_key, _value, e.min = e.min.key, e.min.value, newRsVEBTreeMinMax(_key,_value)
+			_key, _value, e.min = e.min.key, e.min.value, newRsVEBTreeMinMax(_key, _value)
 		}
 
 		if e.u > 2 {
@@ -58,26 +80,15 @@ func (e *rsVEBTreeElement) insert(key, value interface{}) {
 		if _key == e.max.key {
 			e.max.value.PushBack(_value)
 		} else if e.Less(e.max.key, _key) {
-			e.max = newRsVEBTreeMinMax(_key,_value)
+			e.max = newRsVEBTreeMinMax(_key, _value)
 		}
 
 	}
 }
 
-type rsVEBTreeIf interface {
-	createElement() *rsVEBTreeElement
-}
-
-type rsVEBTree struct {
-	root rsVEBTreeElement
-	rsVEBTreeIf
-}
-
-func (t *rsVEBTree) Init(self rsVEBTreeIf, u int) *rsVEBTree {
-	t.rsVEBTreeIf = self
-	if u < 2 {
-		panic("u must be more than 2!")
-	}
-	t.root = *t.rsVEBTreeIf.createElement()
-	return t
+func newRsVEBTreeElement(u int,
+	Less func(interface{}, interface{}) bool,
+	High func(int, interface{}) interface{},
+	Low func(int, interface{}) interface{}) *rsVEBTreeElement {
+	return new(rsVEBTreeElement).init(u, Less, High, Low)
 }
