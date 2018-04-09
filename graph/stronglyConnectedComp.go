43ff18f77bdb34ebd6931a05f8dc698f9dbf6562
Ubuntu <shady831213>
2018-04-08 20:28:26 -0700

    scc use sub graph as components

--- graph/stronglyConnectedComp.go
+++ graph/stronglyConnectedComp.go
@@ -23,20 +23,25 @@ func SCC(g Graph) (scc Graph) {
 	for i := range dfsForest {
 		components[i] = CreateGraphByType(g)
 		for _,v := range dfsForest[i].AllVertices() {
+			//add all sub vertices
 			components[i].AddVertex(v.(*DFSElement).V)
+			//add all sub tree edges
 			for _, e := range dfsForest[i].AllConnectedVertices(v) {
 				components[i].AddEdge(Edge{e.(*DFSElement).V, v.(*DFSElement).V})
 			}
+			//add all sub forward edges
 			for _, e := range dfsGraphOfT["dfsForwardEdges"].AllConnectedVertices(v) {
 				if e.(*DFSElement).FindRoot() == i {
 					components[i].AddEdge(Edge{e.(*DFSElement).V, v.(*DFSElement).V})
 				}
 			}
+			//add all sub back edges
 			for _, e := range dfsGraphOfT["dfsBackEdges"].AllConnectedVertices(v) {
 				if e.(*DFSElement).FindRoot() == i {
 					components[i].AddEdge(Edge{e.(*DFSElement).V, v.(*DFSElement).V})
 				}
 			}
+			//add all sub cross edges
 			for _, e := range dfsGraphOfT["dfsCrossEdges"].AllConnectedVertices(v) {
 				if e.(*DFSElement).FindRoot() == i {
 					components[i].AddEdge(Edge{e.(*DFSElement).V, v.(*DFSElement).V})
@@ -44,7 +49,7 @@ func SCC(g Graph) (scc Graph) {
 			}
 		}
 	}
-	//keep all cross edges
+	//keep all cross edges which cross components
 	for _,e := range dfsGraphOfT["dfsCrossEdges"].AllEdges() {
 		if e.End.(*DFSElement).FindRoot() != e.Start.(*DFSElement).FindRoot() {
 			scc.AddEdge(Edge{components[e.End.(*DFSElement).FindRoot()], components[e.Start.(*DFSElement).FindRoot()]})
