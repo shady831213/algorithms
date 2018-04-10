5d6e6307b2bf78925b0211557c14c30f732c1295
Ubuntu <shady831213>
2018-04-09 23:25:39 -0700

    add vertexBCC

--- graph/bioConnectedComp.go
+++ graph/bioConnectedComp.go
@@ -2,10 +2,11 @@ package graph
 
 import (
 	"sort"
+	"container/list"
 )
 
-func getCutsAndBridgesFromComponent(dfsForest *DFSForest)(cuts,bridges Graph) {
-	cuts,bridges = CreateGraphByType(dfsForest.Trees),CreateGraphByType(dfsForest.Trees)
+func getCutsAndBridgesFromComponent(dfsForest *DFSForest) (cuts, bridges Graph) {
+	cuts, bridges = CreateGraphByType(dfsForest.Trees), CreateGraphByType(dfsForest.Trees)
 	vertices := dfsForest.AllVertices()
 	lows := make(map[*DFSElement]int)
 	//sort in order of decreasing D. it means from deepest to root
@@ -13,12 +14,26 @@ func getCutsAndBridgesFromComponent(dfsForest *DFSForest)(cuts,bridges Graph) {
 		return vertices[i].(*DFSElement).D > vertices[j].(*DFSElement).D
 	})
 	for i := range vertices {
-		lows[vertices[i].(*DFSElement)] = vertices[i].(*DFSElement).D
 		connections := dfsForest.Trees.AllConnectedVertices(vertices[i])
 		for _, v := range connections {
+			//for leaf vertex
+			if _, ok := lows[v.(*DFSElement)]; !ok {
+				lows[v.(*DFSElement)] = v.(*DFSElement).D
+			}
+			//update back edges
+			for _, bv := range dfsForest.BackEdges.AllConnectedVertices(v) {
+				if bv != v.(*DFSElement).P && bv.(*DFSElement).D < lows[v.(*DFSElement)] {
+					lows[v.(*DFSElement)] = bv.(*DFSElement).D
+				}
+			}
+
+			if _, ok := lows[vertices[i].(*DFSElement)]; !ok {
+				lows[vertices[i].(*DFSElement)] = vertices[i].(*DFSElement).D
+			}
 			if lows[v.(*DFSElement)] < lows[vertices[i].(*DFSElement)] {
 				lows[vertices[i].(*DFSElement)] = lows[v.(*DFSElement)]
 			}
+
 			if lows[v.(*DFSElement)] >= vertices[i].(*DFSElement).D {
 				//Cuts, excluding root that has less than 2 children
 				if !(vertices[i].(*DFSElement).P == nil && len(connections) < 2) {
@@ -27,12 +42,73 @@ func getCutsAndBridgesFromComponent(dfsForest *DFSForest)(cuts,bridges Graph) {
 			}
 			if lows[v.(*DFSElement)] > vertices[i].(*DFSElement).D {
 				//bridges
-				bridges.AddEdgeBi(Edge{vertices[i],v})
+				bridges.AddEdgeBi(Edge{vertices[i], v})
 			}
 		}
-		for _, v := range dfsForest.BackEdges.AllConnectedVertices(vertices[i]) {
-			if v != vertices[i].(*DFSElement).P && v.(*DFSElement).D < lows[vertices[i].(*DFSElement)] {
-				lows[vertices[i].(*DFSElement)] = v.(*DFSElement).D
+	}
+	return
+}
+
+func VertexBCC(g Graph) (cuts Graph, comps []Graph) {
+	cuts = CreateGraphByType(g)
+	comps = make([]Graph, 0, 0)
+	lows := make(map[interface{}]int)
+	disc := make(map[interface{}]int)
+	children := make(map[interface{}]int)
+	timer := 0
+	vertexStack := list.New()
+	edgeStack := list.New()
+	for _, v := range g.AllVertices() {
+		if _, ok := disc[v]; !ok {
+			timer ++
+			disc[v] = timer
+			lows[v] = disc[v]
+			vertexStack.PushBack(v)
+			for {
+				top := vertexStack.Back().Value
+				for _, e := range g.AllConnectedVertices(top) {
+					if _, ok := disc[e]; !ok {
+						if _, ok := children[top]; !ok {
+							children[top] = 0
+						}
+						children[top]++
+						timer ++
+						disc[e] = timer
+						lows[e] = disc[e]
+						vertexStack.PushBack(e)
+						edgeStack.PushBack(Edge{top, e})
+						break
+					} else if disc[e] < lows[top] {
+						//back edge
+						edgeStack.PushBack(Edge{top, e})
+						lows[top] = disc[e]
+					}
+				}
+				if top == vertexStack.Back().Value {
+					vertexStack.Remove(vertexStack.Back())
+					if vertexStack.Len() != 0 {
+						next := vertexStack.Back().Value
+						if lows[top] < lows[next] {
+							lows[next] = lows[top]
+						}
+						if lows[top] >= disc[next] {
+							if  !(disc[next] == 1 && children[next] < 2) {
+								cuts.AddVertex(next)
+							}
+							comps = append(comps, CreateGraphByType(g))
+							for edgeStack.Len() != 0 {
+								e := edgeStack.Back().Value
+								edgeStack.Remove(edgeStack.Back())
+								comps[len(comps)-1].AddEdgeBi(e.(Edge))
+								if e.(Edge).Start == next && e.(Edge).End == top {
+									break
+								}
+							}
+						}
+					} else {
+						break
+					}
+				}
 			}
 		}
 	}
--- graph/bioConnectedComp_test.go
+++ graph/bioConnectedComp_test.go
@@ -1,6 +1,9 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+	"sort"
+	)
 
 func bccSetupGraph(g Graph) {
 	for i := 0; i < 23; i ++ {
@@ -70,4 +73,95 @@ func TestGetCutsAndBridgesFromComponent(t *testing.T) {
 	cutsExp, bridgesExp := cutsAndBridgeGolden(dfsForest.Trees)
 	checkDFSGraphOutOfOrder(t, cuts, cutsExp)
 	checkDFSGraphOutOfOrder(t, bridges, bridgesExp)
+}
+
+func vertexBCCGolden(g Graph) (cuts Graph, comps []Graph) {
+	cuts = CreateGraphByType(g)
+	comps = make([]Graph, 12, 12)
+	for i := range comps {
+		comps[i] = CreateGraphByType(g)
+	}
+
+	cuts.AddVertex(19)
+	cuts.AddVertex(17)
+	cuts.AddVertex(14)
+	cuts.AddVertex(12)
+	cuts.AddVertex(10)
+	cuts.AddVertex(7)
+	cuts.AddVertex(4)
+	cuts.AddVertex(0)
+
+	comps[0].AddEdgeBi(Edge{0, 1})
+	comps[0].AddEdgeBi(Edge{1, 2})
+	comps[0].AddEdgeBi(Edge{2, 3})
+	comps[0].AddEdgeBi(Edge{3, 0})
+	comps[11].AddEdgeBi(Edge{0, 4})
+	comps[1].AddEdgeBi(Edge{4, 5})
+	comps[1].AddEdgeBi(Edge{4, 6})
+	comps[1].AddEdgeBi(Edge{5, 6})
+	comps[3].AddEdgeBi(Edge{4, 7})
+	comps[3].AddEdgeBi(Edge{4, 8})
+	comps[3].AddEdgeBi(Edge{7, 8})
+	comps[2].AddEdgeBi(Edge{7, 9})
+	comps[10].AddEdgeBi(Edge{4, 10})
+	comps[9].AddEdgeBi(Edge{10, 11})
+	comps[9].AddEdgeBi(Edge{11, 12})
+	comps[9].AddEdgeBi(Edge{12, 13})
+	comps[9].AddEdgeBi(Edge{13, 10})
+	comps[8].AddEdgeBi(Edge{12, 14})
+	comps[7].AddEdgeBi(Edge{14, 15})
+	comps[7].AddEdgeBi(Edge{15, 16})
+	comps[7].AddEdgeBi(Edge{16, 17})
+	comps[7].AddEdgeBi(Edge{17, 14})
+	comps[7].AddEdgeBi(Edge{14, 18})
+	comps[7].AddEdgeBi(Edge{18, 16})
+	comps[5].AddEdgeBi(Edge{17, 19})
+	comps[4].AddEdgeBi(Edge{19, 20})
+	comps[4].AddEdgeBi(Edge{20, 21})
+	comps[4].AddEdgeBi(Edge{21, 19})
+	comps[6].AddEdgeBi(Edge{17, 22})
+	return
+}
+
+func checkBCCGraphOutOfOrder(t *testing.T, g Graph, gGloden Graph) {
+	edges := g.AllEdges()
+	//finish time increase order
+	vertexes := g.AllVertices()
+	sort.Slice(edges, func(i, j int) bool {
+		if 	edges[i].Start.(int) == edges[j].Start.(int) {
+			return edges[i].End.(int) < edges[j].End.(int)
+		}
+		return edges[i].Start.(int) < edges[j].Start.(int)
+	})
+
+	sort.Slice(vertexes, func(i, j int) bool {
+		return vertexes[i].(int) < vertexes[j].(int)
+	})
+
+	expEdges := gGloden.AllEdges()
+	expVertices := gGloden.AllVertices()
+
+	sort.Slice(expEdges, func(i, j int) bool {
+		if 	expEdges[i].Start.(int) == expEdges[j].Start.(int) {
+			return expEdges[i].End.(int) < expEdges[j].End.(int)
+		}
+		return expEdges[i].Start.(int) < expEdges[j].Start.(int)
+	})
+
+	sort.Slice(expVertices, func(i, j int) bool {
+		return expVertices[i].(int) < expVertices[j].(int)
+	})
+
+	compareGraph(t, vertexes, expVertices, edges, expEdges)
+}
+
+func TestVertexBCC(t *testing.T) {
+	g := NewAdjacencyList()
+	bccSetupGraph(g)
+	cuts, comps := VertexBCC(g)
+	cutsExp, compsExp := vertexBCCGolden(g)
+	checkBCCGraphOutOfOrder(t, cuts, cutsExp)
+	for i := range comps {
+		checkBCCGraphOutOfOrder(t, comps[i], compsExp[i])
+	}
 }
