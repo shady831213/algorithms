efa512796bf3048979f598d2d7599d098e6558d2
Ubuntu <shady831213>
2018-04-10 03:57:46 -0700

    refactory dir

--- heap/linkedHeap_test.go
+++ heap/linkedHeap_test.go
@@ -2,20 +2,19 @@ package linkedHeap
 
 import (
 	"testing"
-	"algorithms/heap"
 )
 
 func Test_linkedHeap(t *testing.T) {
 	h := New()
-	heap.TestHeap(t, h)
+	TestHeap(t, h)
 }
 
 func Test_linkedHeapUnion(t *testing.T) {
 	h, h2 := New(),New()
-	heap.TestHeapUnion(t, h,h2)
+	TestHeapUnion(t, h,h2)
 }
 
 func Benchmark_linkedHeap(b *testing.B) {
 	h := New()
-	heap.BenchmarkHeap(b,h)
+	BenchmarkHeap(b,h)
 }
